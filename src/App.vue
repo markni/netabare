6c90dc15efe11600e4c729ffe646c14c00a97636
markni <xixian.ni@gmail.com>
2018-05-08 14:25:11 -0700

    added a basic bar chart

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="left-panel">
+      <chart></chart>
+    </div>
+    <div class="right-panel">
+      <div class="number-block">
+        <div class="number-block-title"></div>
+        <div class="number-block-number">8.9</div>
+      </div>
+      <div class="number-block">
+        <div class="number-block-title"></div>
+        <div class="number-block-number">1715</div>
+      </div>
+      <div class="number-block">
+        <div class="number-block-title"></div>
+        <div class="number-block-number">213</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import chart from "./components/chart.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    chart
   }
-}
+};
 </script>
 
 <style>
+html {
+  box-sizing: border-box;
+  font-size: 16px;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+
+body,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+ol,
+ul {
+  margin: 0;
+  padding: 0;
+  font-weight: normal;
+}
+
+ol,
+ul {
+  list-style: none;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+}
+
+.left-panel {
+  height: 100vh;
+  position: relative;
+  width: 50vw;
+}
+.right-panel {
+  height: 100vh;
+  position: relative;
+  width: 50vw;
+}
+.number-block {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+.number-block-number {
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  font-size: 20vh;
 }
 </style>
---
+++ src/components/chart.vue
@@ -0,0 +1,48 @@
+<template>
+    <canvas ref="chart"></canvas>
+</template>
+<script>
+import Chart from "chart.js";
+
+const options = {
+  legend: {
+    display: false
+  },
+  maintainAspectRatio: false
+};
+
+const data = {
+  labels: Array.from(Array.from(Array(10).keys(), n => n + 1)),
+  datasets: [
+    {
+      label: false,
+      backgroundColor: [
+        "#EF476F",
+        "#FFD166",
+        "#06D6A0",
+        "#118AB2",
+        "#073B4C",
+        "#EF476F",
+        "#FFD166",
+        "#06D6A0",
+        "#118AB2",
+        "#073B4C"
+      ],
+      data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+    }
+  ]
+};
+
+export default {
+  mounted() {
+    this.$nextTick(function() {
+      const ctx = this.$refs.chart.getContext("2d");
+      new Chart(ctx, { type: "bar", data, options });
+    });
+    // Overwriting base render method with actual data.
+  }
+};
+</script>
+
+<style scoped>
+</style>
