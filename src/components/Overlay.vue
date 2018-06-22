bf02e58bb86a3684162c61d098f275629cb93d71
markni <xixian.ni@gmail.com>
2018-06-21 21:33:28 -0700

    added std lines

--- src/components/Overlay.vue
+++ src/components/Overlay.vue
@@ -38,7 +38,7 @@ export default {
   left: 0;
   right: 0;
   bottom: 0;
-  background: rgba(255, 255, 255, 0.5);
+  background: rgba(255, 255, 255, 1);
   display: flex;
   align-items: center;
   justify-content: center;
