520c0a331ce49a8f27d0f369ca5f8cc97d5b01b6
markni <xixian.ni@gmail.com>
2018-06-24 09:20:01 -0700

    replace user chart with highchart

---
+++ src/components/UserRatingH.vue
@@ -0,0 +1,143 @@
+<template>
+    <div class="chart-container" ref="container">
+    </div>
+</template>
+<script>
+import { PINK, COLORS } from '@/constants/colors';
+import _ from 'lodash';
+import moment from 'moment';
+import Highcharts from 'highcharts';
+
+
+export default {
+  data() {
+    return {
+      chart: null,
+      raw: []
+    };
+  },
+  watch: {
+    UIData: function() {
+      this._refresh();
+    }
+  },
+  methods: {
+    _refresh: function() {
+      console.log(this.UIData);
+      if (this.chart && this.UIData.data) {
+        this.chart.series[0].setData(this.UIData.data);
+      }
+      if (this.chart && this.UIData.data && this.chart.series[1].visible) {
+        this.chart.series[1].setData(this.UIData.gData);
+      }
+    }
+  },
+  props: ['bgmId', 'UIData'],
+  mounted() {
+    this.$nextTick(function() {
+      Highcharts.setOptions({
+        lang: {
+          thousandsSep: ''
+        }
+      });
+      this.chart = Highcharts.chart(this.$refs.container, {
+        chart: {
+          type: 'column'
+        },
+        title: {
+          text: '',
+          enabled: false
+        },
+        tooltip: {
+          backgroundColor: 'black',
+          borderColor: 'none',
+          style: {
+            color: 'white'
+          },
+          formatter: function() {
+            return `${this.x}分：<b>${this.y}</b>`;
+          }
+        },
+        subtitle: {
+          enabled: false
+        },
+        plotOptions: {
+          column: {
+            colorByPoint: true,
+            pointPadding: 0,
+            dataLabels: {
+              enabled: false
+            }
+          }
+        },
+        yAxis: {
+          title: {
+            enabled: false
+          },
+          labels: {
+            format: '{value:.0f}'
+          }
+        },
+        xAxis: {
+
+        },
+        exporting: {
+          enabled: false
+        },
+        legend: {
+          layout: 'vertical',
+          align: 'right',
+          verticalAlign: 'middle',
+          enabled: false
+        },
+        credits: {
+          enabled: false
+        },
+        series: [
+          {
+            name: '个人评分',
+            data: [],
+            colors: COLORS,
+          },
+          {
+            name: '全站评分',
+            data: [],
+            colors: ['rgba(0, 0, 0, 0.2)'],
+            visible: false,
+          }
+        ],
+        colors: COLORS,
+        responsive: {
+          rules: [
+            {
+              condition: {
+                maxWidth: 500
+              },
+              chartOptions: {
+                legend: {
+                  layout: 'horizontal',
+                  align: 'center',
+                  verticalAlign: 'bottom'
+                }
+              }
+            }
+          ]
+        }
+      });
+      if (this.UIData.data)
+        this._refresh();
+    });
+  },
+  updated() {
+    this._refresh();
+  }
+};
+</script>
+
+<style scoped>
+.chart-container {
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+}
+</style>
