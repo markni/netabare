a038960c3673d49a66c6a653bd462a4ff3dd1421
mark <xixian.ni@gmail.com>
2024-03-24 01:40:37 -0700

    feat(User.vue): adjust height calculation in media query #2455b9f
    The height calculation within the media query for screens with a minimum width of 901px has been updated. The height of the wrapper class has been changed from a fixed 100vh to a calculated value of 100vh - 83px, providing more accurate layout dimensions.

--- src/views/User.vue
+++ src/views/User.vue
@@ -95,6 +95,7 @@ export default {
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
 }
+
 @media only screen and (min-width: 901px) {
   .wrapper {
     display: flex;
