a6faf275b5dca2a9be65fbfc1d7b6cfd32304275
mark <xixian.ni@gmail.com>
2024-04-20 20:31:05 -0700

    ✨ added licenses

---
+++ scripts/licenses.js
@@ -0,0 +1,31 @@
+import fs from 'fs/promises'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+// Convert the URL to a path
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+// Assuming the JSON file is in the root and the script in 'scripts/'
+const inputFile = path.join(__dirname, '..', 'licenses.json')
+const outputFile = path.join(__dirname, '..', 'licenses.json')
+
+async function sanitizeJson() {
+  try {
+    // Read the JSON file
+    const data = await fs.readFile(inputFile)
+    const json = JSON.parse(data)
+
+    // Remove the 'path' key from each entry
+    for (const packageName of Object.keys(json)) {
+      delete json[packageName].path
+    }
+
+    // Write the modified data back to a new JSON file
+    await fs.writeFile(outputFile, JSON.stringify(json, null, 4))
+    console.log(`Sanitized JSON has been saved to '${outputFile}'`)
+  } catch (err) {
+    console.error('Error:', err)
+  }
+}
+
+sanitizeJson()
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,63 @@
 <template>
   <div
-    class="flex flex-col h-full w-full items-center justify-center text-6xl tracking-wider gap-16"
+    class="flex flex-col h-full w-full items-center justify-center text-6xl tracking-wider gap-64"
   >
-    <div class="text-4xl">Netabare</div>
+    <div class="flex flex-col gap-16">
+      <h1 class="text-4xl text-center">Netabare</h1>
 
-    <div class="flex flex-col gap-4 tracking-widest">
-      <div class="gap-10 flex items-baseline">
-        <div class="text-4xl">当季动画</div>
-        <RouterLink class="text-5xl hover:bg-mint" to="/season">龙争虎斗</RouterLink>
-      </div>
+      <div class="flex flex-col gap-4 tracking-widest">
+        <h2 class="gap-10 flex items-baseline justify-between">
+          <span class="text-4xl">当季动画</span>
+          <RouterLink class="text-5xl hover:bg-mint" to="/season">龙争虎斗</RouterLink>
+        </h2>
 
-      <div class="gap-10 flex items-baseline">
-        <div class="text-4xl">全站动画</div>
-        <RouterLink class="text-5xl hover:bg-blue" to="/trending">热门趋势</RouterLink>
-      </div>
+        <h2 class="gap-10 flex items-baseline justify-between">
+          <span class="text-4xl">全站动画</span>
+          <RouterLink class="text-5xl hover:bg-blue" to="/trending">热门趋势</RouterLink>
+        </h2>
 
-      <div class="gap-10 flex items-baseline">
-        <div class="text-4xl">全站动画</div>
-        <RouterLink class="text-5xl hover:bg-pink" to="/history">评分分布图</RouterLink>
+        <h2 class="gap-10 flex items-baseline justify-between">
+          <span class="text-4xl">全站动画</span>
+          <RouterLink class="text-5xl hover:bg-pink" to="/history">评分分布图</RouterLink>
+        </h2>
+        <h2 class="gap-10 flex items-baseline justify-between">
+          <span class="text-4xl">个人动画</span>
+          <RouterLink class="text-5xl hover:bg-gold" to="/user">评分分布</RouterLink>
+        </h2>
       </div>
-      <div class="gap-10 flex items-baseline">
-        <div class="text-4xl">个人动画</div>
-        <RouterLink class="text-5xl hover:bg-gold" to="/user">评分分布</RouterLink>
+    </div>
+
+    <div class="flex flex-col gap-16" v-if="licenses">
+      <h2 class="text-4xl text-center">Credits</h2>
+
+      <div class="flex flex-col gap-4 tracking-widest">
+        <div
+          v-for="(value, key) in licenses"
+          :key="key"
+          class="gap-10 flex items-baseline justify-between"
+        >
+          <a :href="value.repository" class="text-2xl hover:bg-blue">{{ key }}</a>
+          <a :href="value.repository" class="text-3xl hover:bg-blue">{{ value.publisher }}</a>
+        </div>
       </div>
+    </div>
+
+    <div class="flex flex-col gap-16" v-if="licenses">
+      <h2 class="text-4xl text-center">Special Thanks</h2>
+
+      <div class="flex flex-col gap-4 tracking-widest">
+        <div class="gap-10 flex items-baseline justify-between">
+          <div class="text-4xl">数据支持</div>
+          <a href="https://bgm.tv" target="_blank" class="text-5xl hover:bg-pink">bangumi.tv</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex flex-col gap-16 mt-32 mb-64" v-if="licenses">
+      <h2 class="text-4xl text-center">Netabare制作委员会</h2>
     </div>
   </div>
 </template>
-<style scoped></style>
-<script setup></script>
+<script setup>
+import licenses from '../../licenses.json'
+</script>
