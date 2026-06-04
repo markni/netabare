87e8f18a19b849c77a93881fae75c520a1d61cac
Mark Ni <xixian.ni@gmail.com>
2026-06-04 00:44:59 -0700

    Adjust dark footer background

--- src/App.vue
+++ src/App.vue
@@ -90,7 +90,7 @@ console.log(`
       <div :class="['bottom-0 mx-auto w-full max-w-6xl p-4 pt-6']">
         <RouterView />
       </div>
-      <footer class="mt-auto bg-black text-white dark:bg-white dark:text-black">
+      <footer class="mt-auto bg-black text-white dark:bg-neutral-800 dark:text-white">
         <div class="mx-auto w-full max-w-6xl px-4 py-24 sm:py-32">
           <div class="flex flex-wrap items-center gap-4 text-sm">
             <RouterLink to="/">{{ texts._backToHome }}</RouterLink>
